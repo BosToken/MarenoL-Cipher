c483c07b6da8509086c89d817f775571406a0d4d
BosToken <faizdiandra11@gmail.com>
2022-10-12 21:00:59 +0700

    update

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from decrypt import dec
 from encrypt import enc
 
+
 #Input 
-user_input = str(input('[+] Enter Your [txt] File : '))
+
+# user_choose = str(input('1. Encryption\n2. Decryption\nChoose : '))
+# user_input = str(input('[+] Enter Your [txt] File : '))
 
 try:
+    
+    #Input 
+    user_choose = int(input('1. Encryption\n2. Decryption\n\nChoose : '))
+
+    if(user_choose == 1):
+        user_input = str(input('[+] Enter Your [txt] File to Encryption : '))
+        enc(user_input)
+
+    if(user_choose == 2):
+        user_input = str(input('[+] Enter Your [txt] File to Decryption : '))
+        dec(user_input)
+
     file = open(user_input)
     file.close
-    enc(user_input)
-    # dec(user_input)
 except FileNotFoundError:
     print("File '"+user_input+"' Not Found")
